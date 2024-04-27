a28e4d776f124c1959c1995544b1bae2a35316d9
kmd192 <kmd192@naver.com>
2024-04-27 16:07:55 +0900

    Optional 관련 버그 수정, get 대신 orElse

--- src/main/java/com/exam/sbb/controller/MainController.java
+++ src/main/java/com/exam/sbb/controller/MainController.java
@@ -171,7 +171,7 @@ public class MainController {
                 .stream()
                 .filter(a -> a.getId() == id)
                 .findFirst()
-                .get();
+                .orElse(null);
 
         return article;
     }
@@ -184,7 +184,7 @@ public class MainController {
                 .stream()
                 .filter(a -> a.getId() == id)
                 .findFirst()
-                .get();
+                .orElse(null);
 
         if(article == null){
             return "%d번 게시물은 존재하지 않습니다.".formatted(id);
