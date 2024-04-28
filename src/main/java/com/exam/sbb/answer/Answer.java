789f88cf1e431938ce41fc3b505590f4394f8ee9
kmd192 <kmd192@naver.com>
2024-04-28 20:54:05 +0900

    Answer 엔티티 추가

--- src/main/java/com/exam/sbb/answer/Answer.java
+++ src/main/java/com/exam/sbb/answer/Answer.java
@@ -1,5 +1,6 @@
-package com.exam.sbb.question;
+package com.exam.sbb.answer;
 
+import com.exam.sbb.question.Question;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,18 +9,17 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
-@Entity // 아래 Question 클래스는 엔티티 클래스이다.
-// 아래 클래스와 1:1로 매칭되는 테이블이 DB에 없다면, 자동으로 생성되어야 한다.
-public class Question {
+@Entity
+public class Answer {
     @Id //primary key
     @GeneratedValue(strategy = GenerationType.IDENTITY) // auto increment
     private Integer id;
-
-    @Column(length = 200) // varchar(200)
-    private String subject;
 
     @Column(columnDefinition = "TEXT")
     private String content;
 
     private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
 }
