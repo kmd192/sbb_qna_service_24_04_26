278dfc5c2e0acdc22b8fade8242023889c64c6d2
kmd192 <kmd192@naver.com>
2024-04-28 23:29:28 +0900

    findBySubjectLike

--- src/test/java/com/exam/sbb/SbbApplicationTests.java
+++ src/test/java/com/exam/sbb/SbbApplicationTests.java
@@ -52,4 +52,17 @@ class SbbApplicationTests {
 		System.out.println(q);//해당 subject 없으면 null인데 exception 발생안하네?
 	}
 
+	@Test
+	void testJpa4() {
+		Question q = questionRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+		assertEquals(1,q.getId());
+	}
+
+	@Test
+	void testJpa5() {
+		List<Question> qList = questionRepository.findBySubjectLike("sbb%");
+		Question q = qList.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
+	}
+
 }
