10cc34c69c434da105ca75a46846e0fa07f1022a
kmd192 <kmd192@naver.com>
2024-04-28 22:12:42 +0900

    findAll

--- src/test/java/com/exam/sbb/SbbApplicationTests.java
+++ src/test/java/com/exam/sbb/SbbApplicationTests.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SbbApplicationTests {
@@ -22,15 +24,26 @@ class SbbApplicationTests {
 		q1.setSubject("sbb가 무엇인가요?");
 		q1.setContent("sbb에 대해서 알고 싶습니다.");
 		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1);  // 첫번째 질문 저장
+		questionRepository.save(q1);  // 첫번째 질문 저장
 
 		Question q2 = new Question();
 		q2.setSubject("스프링부트 모델 질문입니다.");
 		q2.setContent("id는 자동으로 생성되나요?");
 		q2.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q2);  // 두번째 질문 저장
+		questionRepository.save(q2);  // 두번째 질문 저장
 
 		assertThat(q1.getId()).isGreaterThan(0);
 		assertThat(q2.getId()).isGreaterThan(q1.getId());
 	}
+
+	@Test
+	void testJpa2() {
+		// SELECT * FROM question
+		List<Question> all = this.questionRepository.findAll();
+		assertEquals(2, all.size());
+
+		Question q = all.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
+	}
+
 }
