94b3adee3f58fcf9b79518aa2701655b3120c596
kmd192 <kmd192@naver.com>
2024-04-29 18:27:24 +0900

    저장시마다 데이터가 쌓이지 않도록

--- src/main/java/com/exam/sbb/answer/AnswerRepository.java
+++ src/main/java/com/exam/sbb/answer/AnswerRepository.java
@@ -1,6 +1,14 @@
 package com.exam.sbb.answer;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+
+    @Transactional
+    @Modifying
+    @Query(value = "truncate answer", nativeQuery = true)
+    void truncate();
 }
---
+++ src/test/java/com/exam/sbb/AnswerRepositoryTests.java
@@ -0,0 +1,63 @@
+package com.exam.sbb;
+
+import com.exam.sbb.answer.Answer;
+import com.exam.sbb.answer.AnswerRepository;
+import com.exam.sbb.question.Question;
+import com.exam.sbb.question.QuestionRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+class AnswerRepositoryTests {
+
+	@Autowired
+	private QuestionRepository questionRepository;
+
+	@Autowired
+	private AnswerRepository answerRepository;
+	private static int lastSampleDataId;
+
+	@BeforeEach
+	void beforeEach(){
+		clearData();
+		createSampleDate();
+	}
+
+	private void clearData() {
+		questionRepository.disableForeginKeyChecks();
+		answerRepository.truncate();
+		questionRepository.enableForeginKeyChecks();
+	}
+
+	private void createSampleDate() {
+		Question q1 = new Question();
+		q1.setSubject("sbb가 무엇인가요?");
+		q1.setContent("sbb에 대해서 알고 싶습니다.");
+		q1.setCreateDate(LocalDateTime.now());
+		questionRepository.save(q1);  // 첫번째 질문 저장
+
+		Question q2 = new Question();
+		q2.setSubject("스프링부트 모델 질문입니다.");
+		q2.setContent("id는 자동으로 생성되나요?");
+		q2.setCreateDate(LocalDateTime.now());
+		questionRepository.save(q2);  // 두번째 질문 저장
+
+		lastSampleDataId = q2.getId();
+	}
+
+	@Test
+	void testJpa() {
+		Question q = questionRepository.findById(2).get();
+
+		Answer a = new Answer();
+		a.setContent("네 자동으로 생성됩니다.");
+		a.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
+		a.setCreateDate(LocalDateTime.now());
+		answerRepository.save(a);
+	}
+
+}
