eef83dd61fb634fb849b061b848c2e00d82fc41c
kmd192 <kmd192@naver.com>
2024-04-29 20:27:47 +0900

    질문 테스트 데이터 중복 제거

--- src/test/java/com/exam/sbb/AnswerRepositoryTests.java
+++ src/test/java/com/exam/sbb/AnswerRepositoryTests.java
@@ -28,25 +28,15 @@ class AnswerRepositoryTests {
 	}
 
 	private void clearData() {
+		QuestionRepositoryTests.clearData(questionRepository);
+
 		questionRepository.disableForeginKeyChecks();
 		answerRepository.truncate();
 		questionRepository.enableForeginKeyChecks();
 	}
 
 	private void createSampleDate() {
-		Question q1 = new Question();
-		q1.setSubject("sbb가 무엇인가요?");
-		q1.setContent("sbb에 대해서 알고 싶습니다.");
-		q1.setCreateDate(LocalDateTime.now());
-		questionRepository.save(q1);  // 첫번째 질문 저장
-
-		Question q2 = new Question();
-		q2.setSubject("스프링부트 모델 질문입니다.");
-		q2.setContent("id는 자동으로 생성되나요?");
-		q2.setCreateDate(LocalDateTime.now());
-		questionRepository.save(q2);  // 두번째 질문 저장
-
-		lastSampleDataId = q2.getId();
+		QuestionRepositoryTests.createSampleData(questionRepository);
 	}
 
 	@Test
--- src/test/java/com/exam/sbb/QuestionRepositoryTests.java
+++ src/test/java/com/exam/sbb/QuestionRepositoryTests.java
@@ -22,7 +22,7 @@ class QuestionRepositoryTests {
 	@BeforeEach
 	void beforeEach(){
 		clearData();
-		createSampleDate();
+		createSampleData();
 	}
 
 	private void clearData() {
@@ -31,7 +31,13 @@ class QuestionRepositoryTests {
 		questionRepository.enableForeginKeyChecks();
 	}
 
-	private void createSampleDate() {
+	public static void clearData(QuestionRepository questionRepository) {
+		questionRepository.disableForeginKeyChecks();
+		questionRepository.truncate();
+		questionRepository.enableForeginKeyChecks();
+	}
+
+	public static int createSampleData(QuestionRepository questionRepository) {
 		Question q1 = new Question();
 		q1.setSubject("sbb가 무엇인가요?");
 		q1.setContent("sbb에 대해서 알고 싶습니다.");
@@ -44,7 +50,11 @@ class QuestionRepositoryTests {
 		q2.setCreateDate(LocalDateTime.now());
 		questionRepository.save(q2);  // 두번째 질문 저장
 
-		lastSampleDataId = q2.getId();
+		return q2.getId();
+	}
+
+	public void createSampleData() {
+		lastSampleDataId = createSampleData(questionRepository);
 	}
 
 	@Test
