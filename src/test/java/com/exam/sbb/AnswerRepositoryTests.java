eb1c0d7ce590e305992fb2c39ba5387a63c68913
kmd192 <kmd192@naver.com>
2024-04-29 22:14:50 +0900

    답변을 가져올 때는 관련된 질문도 같이 가져오도록

--- src/test/java/com/exam/sbb/AnswerRepositoryTests.java
+++ src/test/java/com/exam/sbb/AnswerRepositoryTests.java
@@ -75,4 +75,11 @@ class AnswerRepositoryTests {
 		assertThat(a.getContent()).isEqualTo("sbb는 질문답변 게시판입니다.");
 	}
 
+	@Test
+	void 관련된_question_조회() {
+		Answer a = answerRepository.findById(1).get();
+		Question q = a.getQuestion();
+		assertThat(q.getId()).isEqualTo(1);
+	}
+
 }
