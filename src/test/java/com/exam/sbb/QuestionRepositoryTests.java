540b178ba2e3168f5974bc00221358268cbd8daa
kmd192 <kmd192@naver.com>
2024-05-03 23:57:17 +0900

    Pageable을 인자로 받는 findAll 리포지터리 테스트

--- src/test/java/com/exam/sbb/QuestionRepositoryTests.java
+++ src/test/java/com/exam/sbb/QuestionRepositoryTests.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -127,89 +130,13 @@ class QuestionRepositoryTests {
 		assertThat(q.getSubject()).isEqualTo("sbb가 무엇인가요?");
 	}
 
-	/*@Test
-	void testJpa0(){
-		Question q1 = new Question();
-		q1.setSubject("sbb가 무엇인가요?");
-		q1.setContent("sbb에 대해서 알고 싶습니다.");
-		q1.setCreateDate(LocalDateTime.now());
-		questionRepository.save(q1);  // 첫번째 질문 저장
-
-		Question q2 = new Question();
-		q2.setSubject("스프링부트 모델 질문입니다.");
-		q2.setContent("id는 자동으로 생성되나요?");
-		q2.setCreateDate(LocalDateTime.now());
-		questionRepository.save(q2);  // 두번째 질문 저장
-
-		questionRepository.disableForeginKeyChecks();
-		questionRepository.truncate();
-		questionRepository.enableForeginKeyChecks();
-	}
-
 	@Test
-	void testJpa1() {
-		Question q1 = new Question();
-		q1.setSubject("sbb가 무엇인가요?");
-		q1.setContent("sbb에 대해서 알고 싶습니다.");
-		q1.setCreateDate(LocalDateTime.now());
-		questionRepository.save(q1);  // 첫번째 질문 저장
+	void Pageable() {
+		// Pageable : 한 페이지에 몇 개의 아이템이 나와야 하는지 + 현재 몇 페이지인지
+		Pageable pageable = PageRequest.of(0, 1);
 
-		Question q2 = new Question();
-		q2.setSubject("스프링부트 모델 질문입니다.");
-		q2.setContent("id는 자동으로 생성되나요?");
-		q2.setCreateDate(LocalDateTime.now());
-		questionRepository.save(q2);  // 두번째 질문 저장
-
-		assertThat(q1.getId()).isGreaterThan(0);
-		assertThat(q2.getId()).isGreaterThan(q1.getId());
+		Page<Question> page = questionRepository.findAll(pageable);
+		assertThat(page.getTotalPages()).isEqualTo(2);
 	}
-
-	@Test
-	void testJpa2() {
-		// SELECT * FROM question
-		List<Question> all = this.questionRepository.findAll();
-		assertEquals(2, all.size());
-
-		Question q = all.get(0);
-		assertEquals("sbb가 무엇인가요?", q.getSubject());
-	}
-
-	@Test
-	void testJpa3() {
-		Question q = questionRepository.findBySubject("sbb가 무엇인가요?");
-		System.out.println(q);//해당 subject 없으면 null인데 exception 발생안하네?
-	}
-
-	@Test
-	void testJpa4() {
-		Question q = questionRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
-		assertEquals(1,q.getId());
-	}
-
-	@Test
-	void testJpa5() {
-		List<Question> qList = questionRepository.findBySubjectLike("sbb%");
-		Question q = qList.get(0);
-		assertEquals("sbb가 무엇인가요?", q.getSubject());
-	}
-
-	@Test
-	void testJpa6() {
-		Optional<Question> oq = questionRepository.findById(1);
-		assertTrue(oq.isPresent());
-		Question q = oq.get();
-		q.setSubject("수정된 제목");
-		this.questionRepository.save(q);
-	}
-
-	@Test
-	void testJpa7() {
-		assertEquals(2, questionRepository.count());
-		Optional<Question> oq = questionRepository.findById(1);
-		assertTrue(oq.isPresent());
-		Question q = oq.get();
-		questionRepository.delete(q);
-		assertEquals(1, questionRepository.count());
-	}*/
 
 }
