f49bd2000b25530e3c9914509c0149fa72237bbb
kmd192 <kmd192@naver.com>
2024-05-03 23:44:52 +0900

    네비게이션 바를 별도의 html 파일로 분리, 대량의 질문 데이터 생성

--- src/test/java/com/exam/sbb/QuestionRepositoryTests.java
+++ src/test/java/com/exam/sbb/QuestionRepositoryTests.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -55,6 +56,21 @@ class QuestionRepositoryTests {
 
 	public void createSampleData() {
 		lastSampleDataId = createSampleData(questionRepository);
+	}
+
+	@Test
+	void createManySampleData(){
+		boolean run = true;
+
+		if(run == false) return;
+
+		IntStream.rangeClosed(3,300).forEach(id -> {
+			Question q = new Question();
+			q.setSubject("%d번 질문".formatted(id));
+			q.setContent("%d번 질문의 내용".formatted(id));
+			q.setCreateDate(LocalDateTime.now());
+			questionRepository.save(q);
+		});
 	}
 
 	@Test
