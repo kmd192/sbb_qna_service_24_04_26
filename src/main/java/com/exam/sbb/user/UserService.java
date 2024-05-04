c4c439e33ba60c3bc5165ee41e4d5bc85cf8fd64
kmd192 <kmd192@naver.com>
2024-05-04 20:32:50 +0900

    PasswordEncoder객체의 생성을 스프링이 관리하도록

--- src/main/java/com/exam/sbb/user/UserService.java
+++ src/main/java/com/exam/sbb/user/UserService.java
@@ -1,7 +1,7 @@
 package com.exam.sbb.user;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -9,12 +9,14 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    // 스프링이 책임지고 PasswordEncoder 타입의 객체를 만들어야하는 상황
+    private final PasswordEncoder passwordEncoder;
 
     public SiteUser create(String username, String email, String password) {
         SiteUser user = new SiteUser();
         user.setUsername(username);
         user.setEmail(email);
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        //BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         user.setPassword(passwordEncoder.encode(password));
         this.userRepository.save(user);
         return user;
