6238bdd2f07d0f49b46fcd12626ca97cea34007d
kmd192 <kmd192@naver.com>
2024-05-05 11:00:09 +0900

    시큐리티를 통한 로그인 폼 처리

--- src/main/java/com/exam/sbb/user/UserRepository.java
+++ src/main/java/com/exam/sbb/user/UserRepository.java
@@ -2,7 +2,11 @@ package com.exam.sbb.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<SiteUser, Long> {
 
     boolean existsByUsername(String username);
+
+    Optional<SiteUser> findByusername(String username);
 }
